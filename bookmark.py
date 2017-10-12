63c2f0c143e934b5a53f74886a3db9b9bd0b7099
Ayano Tobaru <ayano.tobaru@mixi.co.jp>
2017-10-12 09:49:06 +0900

    途中だけど引き継ぎ

--- bookmark.py
+++ bookmark.py
@@ -1,24 +1,54 @@
 import urllib.request
+import re
 from bs4 import BeautifulSoup
 
-userid = 360743
-ncategory = 1
-url = ("http://mypage.syosetu.com/mypagefavnovelmain/list/userid/" +
-       str(userid)+ "/?nowcategory=" +
-       str(ncategory))
 
-response = urllib.request.urlopen(url)
+def get_all_bookmarks(userid, category):
+    url = ("http://mypage.syosetu.com/mypagefavnovelmain/list/userid/" +
+           str(userid) + "index.php?nowcategory=" +
+           str(category) + "&p=1")
+    response = urllib.request.urlopen(url)
+    soup = BeautifulSoup(response, "html.parser")
 
-soup = BeautifulSoup(response, "html.parser")
+    page_count_html = str(soup.find("div", class_="pager_kazu"))
+    pattern_digit = r"^[0-9]+$"
+    m = pattern_digit.search(page_count_html, 0)
+    print(page_count_html)
+    print(m)
+    page_count_num = page_count_html[m.start():m.end()]
+    print(page_count_html)
+    print(page_count_num)
 
-# bookmarks = soup.find_all("div", id="novellist")
-# print(bookmarks)
-# print(type(bookmarks))
 
-# links = bookmarks.find_all("li", class_="title")
+def bookmarks_from_page(userid, category, page):
+    url = ("http://mypage.syosetu.com/mypagefavnovelmain/list/userid/" +
+           str(userid) + "index.php?nowcategory=" +
+           str(category) + "&p=" + str(page))
 
-links = soup.find_all("li", class_="title")
-# print(links)
+    print(url)
 
-for link in links:
-    print (link.find("a").get("href"))
+    response = urllib.request.urlopen(url)
+
+    soup = BeautifulSoup(response, "html.parser")
+
+    # bookmarks = soup.find_all("div", id="novellist")
+    # print(bookmarks)
+    # print(type(bookmarks))
+
+    # links = bookmarks.find_all("li", class_="title")
+
+    links = soup.find_all("li", class_="title")
+    # print(links)
+
+    bookmarks = []
+    for link in links:
+        full_url = link.find("a").get("href")
+        ncode = full_url.replace("https://ncode.syosetu.com/", "").replace("/", "")
+        bookmarks.append(ncode)
+
+    print(bookmarks)
+
+
+if __name__ == "__main__":
+    # bookmarks_from_page(userid=360743, category=1, page=1)
+    get_all_bookmarks(userid=360743, category=1)

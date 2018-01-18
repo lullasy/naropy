b7d2d1d60a92bd6580cb094837c9a47720a76efa
lullasy <cid378@gmail.com>
2018-01-18 12:03:54 +0900

    ブクマ総数を取れるようにした+そのページのブクマ一覧idを取れるようにした

--- bookmark.py
+++ bookmark.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 from bs4 import BeautifulSoup
 
 userid = 360743
@@ -9,7 +10,7 @@ url = ("http://mypage.syosetu.com/mypagefavnovelmain/list/userid/" +
 
 response = urllib.request.urlopen(url)
 
-soup = BeautifulSoup(response, "html.parser")
+soup = BeautifulSoup(response, "lxml")
 
 # bookmarks = soup.find_all("div", id="novellist")
 # print(bookmarks)
@@ -17,8 +18,13 @@ soup = BeautifulSoup(response, "html.parser")
 
 # links = bookmarks.find_all("li", class_="title")
 
-links = soup.find_all("li", class_="title")
-# print(links)
+all_count_raw = str(soup.find("span", class_="allcount").string)
+total_bookmarks = re.search("\d+", all_count_raw).group()
+print(total_bookmarks)
 
-for link in links:
-    print (link.find("a").get("href"))
+re_novel_id = re.compile("https://ncode.syosetu.com/[a-z0-9]+/")
+links = str(soup.find_all("li", class_="title"))
+list_bookmarks = re_novel_id.findall(links)
+print(list_bookmarks)
+for bookmark in list_bookmarks:
+    print (bookmark[26:-1])

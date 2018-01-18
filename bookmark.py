9d0c26d57248f5818a41b10db11117c8ea788e3f
lullasy <cid378@gmail.com>
2018-01-18 12:32:30 +0900

    前のとあわせて、ブクマを全部取得できるようにした。キャストまわりがきたなくて嫌。

--- bookmark.py
+++ bookmark.py
@@ -8,28 +8,26 @@ def get_all_bookmarks(userid, category):
            str(userid) + "index.php?nowcategory=" +
            str(category) + "&p=1")
     response = urllib.request.urlopen(url)
-    page_count_html = str(soup.find("div", class_="pager_kazu"))
-    pattern_digit = r"^[0-9]+$"
-    m = pattern_digit.search(page_count_html, 0)
-    print(page_count_html)
-    print(m)
-    page_count_num = page_count_html[m.start():m.end()]
-    print(page_count_html)
-    print(page_count_num)
-
+    soup = BeautifulSoup(response, "html.parser")
     all_count_raw = str(soup.find("span", class_="allcount").string)
-    total_bookmarks = re.search("\d+", all_count_raw).group()
-    print(total_bookmarks)
+    total_bookmarks = int(re.search("\d+", all_count_raw).group())
+
+    # TODO: 全部のブクマ数がわかるので、10で割ってpager_kazuを無視する！
+    all_bookmarks = []
+    for page in range(int(total_bookmarks / 10)):
+        now_bookmarks = bookmarks_from_page(userid=userid,
+                                            category=category,
+                                            page=page)
+        all_bookmarks.extend(now_bookmarks)
+
+    return all_bookmarks
+
 
 def bookmarks_from_page(userid, category, page):
     url = ("http://mypage.syosetu.com/mypagefavnovelmain/list/userid/" +
            str(userid) + "index.php?nowcategory=" +
            str(category) + "&p=" + str(page))
-
-    print(url)
-
     response = urllib.request.urlopen(url)
-
     soup = BeautifulSoup(response, "html.parser")
 
     # bookmarks = soup.find_all("div", id="novellist")
@@ -44,12 +42,15 @@ def bookmarks_from_page(userid, category, page):
     bookmarks = []
     for link in links:
         full_url = link.find("a").get("href")
-        ncode = full_url.replace("https://ncode.syosetu.com/", "").replace("/", "")
+        # TODO: ゴミ、ダメ、直す
+        ncode = full_url[26:-1]
         bookmarks.append(ncode)
 
-    print(bookmarks)
+    # print(bookmarks)
+    return bookmarks
 
 
 if __name__ == "__main__":
     # bookmarks_from_page(userid=360743, category=1, page=1)
     get_all_bookmarks(userid=360743, category=1)
+    bookmarks_from_page(userid=360743, category=1, page=1)
